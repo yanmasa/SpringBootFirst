30ac40c55061850c888628d1d63afe3ca1ba2b75
yanmasa <ueda3ban@gmail.com>
2022-05-14 08:38:52 +0900

    @GetMapping,@RequestParam

--- spring-boot1/src/main/java/com/example/demo/controller/HelloController.java
+++ spring-boot1/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,20 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-	@RequestMapping("/")
-	public String hello() {
+	@GetMapping("/")
+	public String greeting() {
+		return "hello";
+	}
+	
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="message") String message, Model model) {
+		model.addAttribute("sample",message);
 		return "hello";
 	}
 }
